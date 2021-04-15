1f7589b2d9929a37982dd2d813d3be8c4ff8ac44
NIcolasBonamy <n.bonamy@live.fr>
2021-04-15 22:07:22 +0200

    add dailySentences.json file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import dailySentences from './dailySentences.json';
 
 function App() {
+  const nb = Math.floor(Math.random()*dailySentences.length);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {nb}
+      <h1>{dailySentences[nb].author}</h1>
+      <h2>« {dailySentences[nb].sentence} »</h2>
+      <h3>{dailySentences[nb].date}</h3>
     </div>
   );
 }
