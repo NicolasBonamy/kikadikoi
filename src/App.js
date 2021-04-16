19ad23251ecbd9389d17bbe51c2bc5f6a17b5c51
NIcolasBonamy <n.bonamy@live.fr>
2021-04-16 18:07:48 +0200

    add function to get good picture for each author

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import dailySentences from './dailySentences.json';
+import { useState } from "react";
+import "./App.css";
+import dailySentences from "./dailySentences.json";
 
 function App() {
-  const nb = Math.floor(Math.random()*dailySentences.length);
+  const [number, setNumber] = useState([0]);
+  const handleChangeSentence = () => {
+    setNumber(Math.floor(Math.random() * dailySentences.length));
+  };
+  const srcByName = `./Photos/${dailySentences[number].author.charAt(0).toUpperCase() + dailySentences[number].author.substring(1).toLowerCase()}.jpeg`;
+
   return (
     <div className="App">
-      {nb}
-      <h1>{dailySentences[nb].author}</h1>
-      <h2>« {dailySentences[nb].sentence} »</h2>
-      <h3>{dailySentences[nb].date}</h3>
+      <h1>{dailySentences[number].author}</h1>
+      <img src={srcByName} alt='avatar' />
+      <h2>« {dailySentences[number].sentence} »</h2>
+      <h3>{dailySentences[number].date}</h3>
+      <button onClick={handleChangeSentence}>nouvelle phrase</button>
     </div>
   );
 }
